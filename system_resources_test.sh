fdedb26f63d4b8ad467bd82beae9a0a754afbb45
Ayanna Curwen <yanwen2627@gmail.com>
2024-08-15 23:51:14 -0400

    Update system_resources_test.sh

--- system_resources_test.sh
+++ system_resources_test.sh
@@ -1,25 +1,17 @@
-#!/bin/bash
-
-# Checking CPU usage
-
-# Isolating idle CPU from top output
-idle_cpu=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/")
-echo "Idle CPU: $idle_cpu"
-
-# Removing decimal point to convert it to an integer
-no_id=$(echo "$idle_cpu" | awk -F '.' '{print $1}')
-echo "Idle CPU as an integer: $no_id"
-
-# Calculating total CPU used 
-cpu_used=$((100 - no_id))
-echo "CPU Used: $cpu_used%"
-
-# Stop processes if CPU exceeds 75.
-if [ "$cpu_used" -ge 75 ]
-        then
-        echo "CPU limit reached. Shutting down process."
-        #stop/kill a process here 
-
-        else 
-        echo "CPU usage is fine."
-fi
+ 1 #!/bin/bash
+ 2 
+ 3 # Checking memory
+ 4 # Isolating used MEM from top output
+ 5 used_mem1=$(top -b -n1 | grep "MiB Mem" | awk '{print $6}')
+ 6
+ 7 # Removing everything after the decimal point to convert it to an integer
+ 8 used_mem2=${used_mem1%.*}
+ 9
+10 # Check if used memory exceeds 900 MiB
+11 if [ "$used_mem2" -gt 900 ]; then
+12   echo "Memory use ("$used_mem2" MiB) exceeds the threshold of 900 MiB. Exiting script."
+13   exit 1
+14 else
+15   echo "Memory use is within limits."
+16 fi
+17
