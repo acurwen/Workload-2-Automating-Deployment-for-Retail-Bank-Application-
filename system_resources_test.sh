f098798dbb57863a308bbdb9315e911eb787e24b
Ayanna Curwen <yanwen2627@gmail.com>
2024-08-16 00:04:08 -0400

    Update system_resources_test.sh
    Had numbers in the script since I copied this over from a test script where I used the nano -l command.

--- system_resources_test.sh
+++ system_resources_test.sh
@@ -1,17 +1,19 @@
- 1 #!/bin/bash
- 2 
- 3 # Checking memory
- 4 # Isolating used MEM from top output
- 5 used_mem1=$(top -b -n1 | grep "MiB Mem" | awk '{print $6}')
- 6
- 7 # Removing everything after the decimal point to convert it to an integer
- 8 used_mem2=${used_mem1%.*}
- 9
-10 # Check if used memory exceeds 900 MiB
-11 if [ "$used_mem2" -gt 900 ]; then
-12   echo "Memory use ("$used_mem2" MiB) exceeds the threshold of 900 MiB. Exiting script."
-13   exit 1
-14 else
-15   echo "Memory use is within limits."
-16 fi
-17
+#!/bin/bash
+
+# Checking memory
+# Isolating used MEM from top output
+
+used_mem1=$(top -b -n1 | grep "MiB Mem" | awk '{print $6}')
+
+# Removing everything after the decimal point to convert it to an integer
+used_mem2=${used_mem1%.*}
+
+# Check if used memory exceeds 900 MiB
+if [ "$used_mem2" -gt 900 ]
+then
+    echo "Memory use ("$used_mem2" MiB) exceeds the threshold of 900 MiB. Exiting script."
+    exit 1
+else
+    echo "Memory use is within limits."
+fi
+
